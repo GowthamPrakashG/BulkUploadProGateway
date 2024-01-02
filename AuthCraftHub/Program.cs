e8aac6f3b3a8065971eb388f21ceb46cd891c5d0
Sathish_Ravichandran <10decoders@DESKTOP-RD0I1G3>
2024-01-02 17:15:27 +0530

    Code cleanup and Added ApplicationDBContext

--- AuthCraftHub/Program.cs
+++ AuthCraftHub/Program.cs
@@ -1,11 +1,22 @@
+using DBUtilityHub.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//Application DB Context => Entity Framework
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
